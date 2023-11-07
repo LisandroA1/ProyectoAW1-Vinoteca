de640be53a12f2d5cfb824c06d36e579b84a627d
root <mgiraudo@ies21.edu.ar>
2023-11-07 10:06:08 -0300

    cambios en clase

--- catalogo.js
+++ catalogo.js
@@ -3,15 +3,105 @@ const catalogo = {
     articulos: [
         {
             //todas las caracteristicas del vino
+            id_articulo: 1,
             nombre: "Catena",
             tipo: "malbec",
-            precio: 5500,
+            precio: 5600,
             Imagen:"./imagenes/catena.jpg",
             descripcion: "",
             medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
         }
-            
+        ,
+        {
+            id_articulo: 2,
+            nombre: "FAMILIA TORRES",
+            tipo: "Malbec",
+            Precio: 7780,
+            Imagen: "./imagenes/familia Torres-removebg-preview.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
+        ,
+        {
+            id_articulo: 3,
+            nombre: "CAMPO VIEJO",
+            tipo: "Malbec",
+            Precio: 2500,
+            Imagen: "./imagenes/Campo Viejo.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
+        ,
+        {
+            id_articulo: 4,
+            nombre: "CHÂTEAU MUSAR",
+            tipo: "Malbec",
+            Precio: 7780,
+            Imagen: "./imagenes/Château Musar.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
+        ,
+        {
+            id_articulo: 5,
+            nombre: "CONCHA Y TORO",
+            tipo: "Malbec",
+            Precio: 7780,
+            Imagen: "./imagenes/Concha y Toro.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
+        ,
+        {
+            id_articulo: 6,
+            nombre: "GAJA",
+            tipo: "Malbec",
+            Precio: 3500,
+            Imagen: "./imagenes/Gaja.jpeg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
         
+        ,
+        {
+            id_articulo: 7,
+            nombre: "GAJA",
+            tipo: "Malbec",
+            Precio: 2500,
+            Imagen: "./imagenes/Planeta.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }
+        
+        ,
+        {
+            id_articulo: 8,
+            nombre: "RUTINI",
+            tipo: "Malbec",
+            Precio: 8500,
+            Imagen: "./imagenes/rutini.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }    
+        
+        ,
+        {
+            id_articulo: 9,
+            nombre: "SYMINGTON",
+            tipo: "Malbec",
+            Precio: 1100,
+            Imagen: "./imagenes/Symington.jpg",
+            descripcion: "",
+            medidas:[{medida: "1L", stock: 20},{medida: "3/4", stock: 5}]
+
+        }    
     ]
 }
 
